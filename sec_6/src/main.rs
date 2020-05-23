bb56ef7d057c795b2e48a6edfcfefc5efc8ff997
tenajima <tenajima@gmail.com>
2020-05-23 10:02:24 +0900

    fin sec 6

--- sec_6/src/main.rs
+++ sec_6/src/main.rs
@@ -1,16 +1,6 @@
 fn main() {
-    fn plus_one(x: Option<i32>) -> Option<i32> {
-        match x {
-            None => None,
-            Some(i) => Some(i + 1),
-        }
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
     }
-
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-    println!("{:?}", five);
-    println!("{:?}", six);
-    println!("{:?}", none);
-    println!("{:?}", Some("hoge"));
 }
