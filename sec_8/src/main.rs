847f9a8ccef9588cdb0fa44660ebb814a839de27
Yuto Mizutani <mizutani@fringe81.com>
2020-05-27 11:24:42 +0900

    practice 1 end

--- sec_8/src/main.rs
+++ sec_8/src/main.rs
@@ -1,5 +1,30 @@
-fn main() {
-    for c in "नमस्ते".bytes() {
-        println!("{}", c);
+use std::collections::HashMap;
+fn practice_1() {
+    let mut map = HashMap::new();
+    let mut v = vec![1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 6];
+    v.sort();
+    let median = v[v.len() / 2];
+    // let modian = 0
+    let mut sum = 0;
+    let mut modian = &v[0];
+    let mut modian_counter = 0;
+    for i in &v {
+        sum += i;
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+        if *count > modian_counter {
+            modian = &i;
+            modian_counter = *count;
+        }
     }
+    println!(
+        "mean: {}, midian: {}, modian: {}",
+        sum as f64 / v.len() as f64,
+        median,
+        *modian
+    );
+    println!("{:?}", map)
+}
+fn main() {
+    practice_1();
 }
